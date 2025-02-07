02bc6abe1252483d8209f22835151a85d482e419
adrianriverof <adrianriverof@gmail.com>
2025-02-07 13:01:22 +0100

    feat: separate by commas and then add the numbers

--- Project/Program.cs
+++ Project/Program.cs
@@ -9,6 +9,15 @@ public class StringCalculator {
 	{
 		if (numbers == "")
 			return 0;
+
+		string[] separated_numbers = numbers.Split(",");
+		int value_to_return = 0;
+		foreach (var number in separated_numbers)
+		{
+			value_to_return += int.Parse(number);
+		}
+
+		return value_to_return;
 		return int.Parse(numbers);
 	}
 }
