0a22045dff87c7dc71fd07fe5107a9a317248ba9
adrianriverof <adrianriverof@gmail.com>
2025-02-08 11:20:36 +0100

    refactor: comment about next changes

--- Project/Program.cs
+++ Project/Program.cs
@@ -10,6 +10,10 @@ public class StringCalculator {
 
 		string[] separatedNumbers;
 		
+		// we need to change this so we can find where the string starts (\n)
+		// and allow to find the separator (surrounded by [])
+		//
+		
 		if (numbers.StartsWith("//"))
 		{
 			string separator = numbers[2].ToString();
